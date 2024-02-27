0f2b1d9a7fe59c3863c805743c344deefbdfce9d
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-02-27 23:31:51 +0800

    0227

--- adk_topon_basic/src/main/java/com/mozhimen/adk/topon/basic/bases/BaseATAdSourceStatusCallback.kt
+++ adk_topon_basic/src/main/java/com/mozhimen/adk/topon/basic/bases/BaseATAdSourceStatusCallback.kt
@@ -12,32 +12,32 @@ import com.mozhimen.basick.utilk.bases.IUtilK
  * @Date 2024/2/23
  * @Version 1.0
  */
-class BaseATAdSourceStatusCallback : ATAdSourceStatusListener, IUtilK {
-    override fun onAdSourceBiddingAttempt(adInfo: ATAdInfo) {
-        Log.i(TAG, "onAdSourceBiddingAttempt: $adInfo")
-    }
-
-    override fun onAdSourceBiddingFilled(adInfo: ATAdInfo) {
-        Log.i(TAG, "onAdSourceBiddingFilled: $adInfo")
-    }
-
-    override fun onAdSourceBiddingFail(adInfo: ATAdInfo, adError: com.anythink.core.api.AdError?) {
-        Log.i(TAG, "onAdSourceBiddingFail Info: $adInfo")
-        if (adError != null) {
-            Log.i(TAG, "onAdSourceBiddingFail error: " + adError.getFullErrorInfo())
-        }
-    }
-
+open class BaseATAdSourceStatusCallback : ATAdSourceStatusListener, IUtilK {
     override fun onAdSourceAttempt(adInfo: ATAdInfo) {
-        Log.i(TAG, "onAdSourceAttempt: $adInfo")
+        Log.i(TAG, "onAdSourceAttempt: 广告源开始加载回调 $adInfo")
     }
 
-    override fun onAdSourceLoadFilled(adInfo: ATAdInfo) {
-        Log.i(TAG, "onAdSourceLoadFilled: $adInfo")
+    override fun onAdSourceBiddingAttempt(adInfo: ATAdInfo) {
+        Log.i(TAG, "onAdSourceBiddingAttempt: 竞价广告源开始竞价回调 $adInfo")
     }
 
     override fun onAdSourceLoadFail(adInfo: ATAdInfo, adError: com.anythink.core.api.AdError) {
-        Log.i(TAG, "onAdSourceLoadFail Info: $adInfo")
-        Log.i(TAG, "onAdSourceLoadFail error: " + adError.getFullErrorInfo())
+        Log.e(TAG, "onAdSourceLoadFail 广告源加载失败回调 Info: $adInfo")
+        Log.e(TAG, "onAdSourceLoadFail 广告源加载失败回调 error: ${adError.getFullErrorInfo()}")
+    }
+
+    override fun onAdSourceBiddingFail(adInfo: ATAdInfo, adError: com.anythink.core.api.AdError?) {
+        Log.e(TAG, "onAdSourceBiddingFail 竞价广告源竞价失败回调 Info: $adInfo")
+        if (adError != null) {
+            Log.e(TAG, "onAdSourceBiddingFail error: ${adError.getFullErrorInfo()}")
+        }
+    }
+
+    override fun onAdSourceLoadFilled(adInfo: ATAdInfo) {
+        Log.e(TAG, "onAdSourceLoadFilled: 广告源广告填充回调 $adInfo")
+    }
+
+    override fun onAdSourceBiddingFilled(adInfo: ATAdInfo) {
+        Log.e(TAG, "onAdSourceBiddingFilled: 竞价广告源竞价成功回调 $adInfo")
     }
 }
--- adk_topon_basic/src/main/java/com/mozhimen/adk/topon/basic/helpers/BannerAdProxy.kt
+++ adk_topon_basic/src/main/java/com/mozhimen/adk/topon/basic/helpers/BannerAdProxy.kt
@@ -1,28 +1,20 @@
 package com.mozhimen.adk.topon.basic.helpers
 
-import android.app.Activity
 import android.content.Context
 import android.util.Log
-import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
-import android.widget.ScrollView
 import androidx.lifecycle.LifecycleOwner
 import com.anythink.banner.api.ATBannerExListener
 import com.anythink.banner.api.ATBannerView
 import com.anythink.core.api.ATAdConst
-import com.anythink.core.api.ATAdInfo
 import com.anythink.core.api.ATAdSourceStatusListener
-import com.anythink.core.api.ATNetworkConfirmInfo
-import com.anythink.core.api.AdError
-import com.mozhimen.adk.topon.basic.utils.MediationNativeAdUtil
 import com.mozhimen.basick.elemk.androidx.lifecycle.bases.BaseWakeBefDestroyLifecycleObserver
 import com.mozhimen.basick.lintk.optins.OApiCall_BindLifecycle
 import com.mozhimen.basick.lintk.optins.OApiCall_BindViewLifecycle
 import com.mozhimen.basick.lintk.optins.OApiInit_ByLazy
 import com.mozhimen.basick.utilk.android.util.UtilKDisplayMetrics
 import com.mozhimen.basick.utilk.android.util.dp2px
-import com.mozhimen.basick.utilk.android.view.UtilKScreen
 import com.mozhimen.basick.utilk.android.view.applyVisible
 
 /**
@@ -35,7 +27,7 @@ import com.mozhimen.basick.utilk.android.view.applyVisible
 @OApiCall_BindViewLifecycle
 @OApiCall_BindLifecycle
 @OApiInit_ByLazy
-class BannerAdProxy(private val _activity: Context, private val _owner: LifecycleOwner) : BaseWakeBefDestroyLifecycleObserver(){
+class BannerAdProxy(private var _activity: Context?, private val _owner: LifecycleOwner) : BaseWakeBefDestroyLifecycleObserver() {
     private var mBannerView: ATBannerView? = null
 
     ///////////////////////////////////////////////////////////////////////
@@ -59,26 +51,28 @@ class BannerAdProxy(private val _activity: Context, private val _owner: Lifecycl
         container.applyVisible()
         val padding = 12f.dp2px.toInt()
         val localMap: MutableMap<String, Any> = HashMap()
-        localMap[ATAdConst.KEY.AD_WIDTH] = UtilKDisplayMetrics.getAppWidthPixels(_activity) - 2 * padding
-        localMap[ATAdConst.KEY.AD_HEIGHT] = 60f.dp2px.toInt()
+        localMap[ATAdConst.KEY.AD_WIDTH] = UtilKDisplayMetrics.getSysWidthPixels() - 2 * padding
+        localMap[ATAdConst.KEY.AD_HEIGHT] = 50f.dp2px.toInt()
         mBannerView!!.setLocalExtra(localMap)
 
         //横幅广告使用原生自渲染广告，只需要在发起请求时额外设置setNativeAdCustomRender即可，请求、展示广告流程同横幅广告接入流程相同。
-        mBannerView!!.setNativeAdCustomRender { mixNativeAd, atAdInfo -> MediationNativeAdUtil.getViewFromNativeAd(_activity, mixNativeAd, atAdInfo, false) }
+//        mBannerView!!.setNativeAdCustomRender { mixNativeAd, atAdInfo -> MediationNativeAdUtil.getViewFromNativeAd(_activity, mixNativeAd, atAdInfo, false) }
         mBannerView!!.loadAd()
     }
 
     fun addBannerViewToContainer(container: ViewGroup) {
         if (mBannerView != null) {
-            container.addView(mBannerView, FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, container.layoutParams.height))
+            container.addView(mBannerView, FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT))
         }
     }
 
     ///////////////////////////////////////////////////////////////////////
 
     override fun onDestroy(owner: LifecycleOwner) {
+        Log.d(TAG, "onDestroy: ")
         mBannerView?.destroy()
         mBannerView = null
+        _activity = null
         super.onDestroy(owner)
     }
 }
